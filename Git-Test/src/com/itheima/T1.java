b0d131245773e39da82c2c721b44b4eeb7148460
lixunhuan <2601070799@qq.com>
2020-06-11 20:29:53 +0800

    增加方法

--- Git-Test/src/com/itheima/T1.java
+++ Git-Test/src/com/itheima/T1.java
@@ -3,12 +3,9 @@ package com.itheima;
 public class T1 {
     public static void main(String[] args) {
         System.out.println("dsad");
-        System.out.println("dsad");
-        System.out.println("dsad");
-        System.out.println("dsad");
-        System.out.println("dsad");
-        System.out.println("dsad");
-        System.out.println("dsad");
-        System.out.println("dsad");
+
+    }
+    void ma(){
+        System.out.println("aaa");
     }
 }
