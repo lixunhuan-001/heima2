ec70d1b20946970e51ccd79d46e3dec6434fb008
lixunhuan <2601070799@qq.com>
2020-06-11 16:47:10 +0800

    第一次提交代码文件

---
+++ Git-Test/src/com/itheima/T1.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class T1 {
+    public static void main(String[] args) {
+        System.out.println("dsad");
+        System.out.println("dsad");
+        System.out.println("dsad");
+        System.out.println("dsad");
+        System.out.println("dsad");
+    }
+}
